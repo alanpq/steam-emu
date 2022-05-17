73160d94abd46e9d7efa76fff1b040554d6bc250
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-18 00:54:41 +0100

    add more logs

--- src/steam_game_server.rs
+++ src/steam_game_server.rs
@@ -1,19 +1,26 @@
 use crate::{int32, HSteamUser, HSteamPipe};
 
+use tracing::{info, debug, error};
 #[no_mangle]
 pub extern "C" fn SteamGameServer_GetHSteamPipe() -> HSteamPipe {
+  debug!("SteamGameServer_GetHSteamPipe");
   0 // FIXME: implement
 }
 
 #[no_mangle]
 pub extern "C" fn SteamGameServer_RunCallbacks() {
+  debug!("SteamGameServer_RunCallbacks");
    // FIXME: implement
 }
 
 #[no_mangle]
 pub extern "C" fn SteamGameServer_GetHSteamUser() -> HSteamUser {
+  debug!("SteamGameServer_GetHSteamUser");
   0 // FIXME: implement
 }
 
 #[no_mangle]
-pub extern "C" fn SteamGameServer_Shutdown() {}
+pub extern "C" fn SteamGameServer_Shutdown() {
+  debug!("SteamGameServer_Shutdown");
+  // FIXME: implement
+}
