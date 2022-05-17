034fa332d16d842b5cede186bd69315797d1621c
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-17 23:58:02 +0100

    up log level for lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{net::TcpStream, sync::Mutex, os::raw::{c_int, c_uint, c_short, c_ushort, c_char, c_uchar, c_ulonglong}};
 
-use tracing::info;
+use tracing::{info, debug, Level};
 
 pub mod steam_game_server;
 pub mod steam_internal;
@@ -9,9 +9,9 @@ pub mod steam_api;
 #[ctor::ctor]
 fn ctor() {
   let stream = TcpStream::connect("127.0.0.1:1337").unwrap();
-  tracing_subscriber::fmt().with_writer(Mutex::new(stream)).init();
+  tracing_subscriber::fmt().with_max_level(Level::DEBUG).with_writer(Mutex::new(stream)).init();
 
-  info!("hi");
+  info!("steam_api.dll loaded!");
 }
 
 pub type int32 = c_int;
