5a6ff603363820ca0d272734e376d64294eb735a
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-18 22:45:36 +0100

    add fast calls + SteamClient_GetISteamFriends

--- src/steam_client/methods.rs
+++ src/steam_client/methods.rs
@@ -6,14 +6,15 @@ use crate::{HSteamPipe, HSteamUser};
 
 use super::{SteamClient, EAccountType};
 
-pub extern "fastcall" fn SteamAPI_ISteamClient_CreateSteamPipe
-  (self_: *mut SteamClient) -> HSteamPipe {
+pub extern "fastcall" fn SteamAPI_ISteamClient_CreateSteamPipe(self_: *mut SteamClient) -> HSteamPipe {
   debug!("SteamAPI_ISteamClient_CreateSteamPipe");
   0
 }
 
-pub extern "fastcall" fn SteamAPI_ISteamClient_BReleaseSteamPipe
-  (self_: *mut SteamClient, hSteamPipe:HSteamPipe) -> bool {
+pub extern "fastcall" fn SteamAPI_ISteamClient_BReleaseSteamPipe(
+  self_: *mut SteamClient,
+  hSteamPipe:HSteamPipe
+) -> bool {
   debug!("SteamAPI_ISteamClient_BReleaseSteamPipe");
   false
 }
@@ -24,31 +25,65 @@ pub extern "fastcall" fn SteamAPI_ISteamClient_ConnectToGlobalUser
   0
 }
 
-pub extern "fastcall" fn SteamAPI_ISteamClient_CreateLocalUser
-  (self_: *mut SteamClient, hSteamPipe: *mut HSteamPipe, eAccountType:EAccountType) -> HSteamUser {
+pub extern "fastcall" fn SteamAPI_ISteamClient_CreateLocalUser(
+  self_: *mut SteamClient,
+  hSteamPipe: *mut HSteamPipe,
+  eAccountType:EAccountType
+) -> HSteamUser {
   debug!("SteamAPI_ISteamClient_CreateLocalUser");
   0
 }
 
-pub extern "fastcall" fn SteamAPI_ISteamClient_ReleaseUser
-  (self_: *mut SteamClient, hSteamPipe:HSteamPipe, hSteamUser:HSteamUser) {
+pub extern "fastcall" fn SteamAPI_ISteamClient_ReleaseUser(
+  self_: *mut SteamClient,
+  hSteamPipe:HSteamPipe,
+  hSteamUser:HSteamUser
+) {
   debug!("SteamAPI_ISteamClient_ReleaseUser");
 }
 
-pub unsafe extern "fastcall" fn SteamAPI_ISteamClient_GetISteamUser(self_: *mut SteamClient, hSteamUser:HSteamUser, hSteamPipe:HSteamPipe, pchVersion: *const c_char) ->  *mut c_void {
+pub unsafe extern "fastcall" fn SteamAPI_ISteamClient_GetISteamUser(
+  self_: *mut SteamClient,
+  hSteamUser:HSteamUser, 
+  hSteamPipe:HSteamPipe, 
+  pchVersion: *const c_char
+) ->  *mut c_void {
   debug!("GetISteamUser");
-  return (*self_).steam_user
+  ptr::null_mut()
+}
+
+pub unsafe extern "C" fn SteamAPI_ISteamClient_GetISteamFriends(
+  self_: *mut SteamClient, 
+  hSteamUser: HSteamUser, 
+  hSteamPipe: HSteamPipe, 
+  pchVersion: *const c_char
+) -> *mut c_void {
+  debug!("GetISteamFriends");
+  ptr::null_mut()
 }
 
 pub fn get_vtable() -> *mut *mut usize {
   unsafe {
-    static mut VTABLE: [*mut usize; 6] =  [
+    static mut VTABLE: [*mut usize; 19] =  [
       SteamAPI_ISteamClient_CreateSteamPipe as _,
       SteamAPI_ISteamClient_BReleaseSteamPipe as _,
       SteamAPI_ISteamClient_ConnectToGlobalUser as _,
       SteamAPI_ISteamClient_CreateLocalUser as _,
       SteamAPI_ISteamClient_ReleaseUser as _,
       SteamAPI_ISteamClient_GetISteamUser as _,
+      ptr::null_mut(),
+      ptr::null_mut(),
+      SteamAPI_ISteamClient_GetISteamFriends as _,
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
     ];
     VTABLE.as_mut_ptr()
   }
