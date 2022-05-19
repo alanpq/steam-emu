0bbb0bfee04e40d557d52a6f3b7e9bfcb21ddbec
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-19 18:02:21 +0100

    modify script for use with spacewar project

--- spacewar.sh
+++ spacewar.sh
@@ -4,7 +4,11 @@ echo "Built new dll."
 cp target/i686-pc-windows-msvc/debug/steam_emu.dll spacewar/steam_api.dll
 cp target/i686-pc-windows-msvc/debug/steam_emu.pdb spacewar/steam_api.pdb
 
+cp target/i686-pc-windows-msvc/debug/steam_emu.dll sdk/redistributable_bin/steam_api.dll
+cp target/i686-pc-windows-msvc/debug/steam_emu.pdb sdk/redistributable_bin/steam_api.pdb
+cp target/i686-pc-windows-msvc/debug/steam_emu.pdb sdk/redistributable_bin/steam_emu.pdb
+
 
 echo "Patched spacewar 'steam_api.dll'"
-echo "Running spacewar..."
-spacewar/SteamworksExample.exe
+# echo "Running spacewar..."
+# spacewar/SteamworksExample.exe
