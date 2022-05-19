074eb2dbc146810d386ab03bec072c65d13ae190
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-19 18:20:57 +0100

    implement ISteamApps_GetLaunchCommandLine

--- src/steam_api/apps.rs
+++ src/steam_api/apps.rs
@@ -4,6 +4,8 @@ use tracing::{info, debug, error};
 use vtables::VTable;
 use vtables_derive::{VTable, has_vtable};
 
+use crate::int32;
+
 #[has_vtable]
 #[derive(VTable, Debug)]
 pub struct SteamApps {
@@ -15,13 +17,45 @@ impl SteamApps {
   }
 }
 
+pub extern "fastcall" fn SteamAPI_ISteamApps_GetLaunchCommandLine(
+  self_: *mut SteamApps,
+  _edx: *mut c_void,
+  pszCommandLine: *mut c_char,
+  cubCommandLine: int32,
+) -> int32 {
+  0
+}
+
 pub fn get_vtable() -> *mut *mut usize {
   unsafe {
-    static mut VTABLE: [*mut usize; 4] = [
+    static mut VTABLE: [*mut usize; 27] = [
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      SteamAPI_ISteamApps_GetLaunchCommandLine as _,
     ];
     VTABLE.as_mut_ptr()
   }
