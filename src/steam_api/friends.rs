0db6a293f5df4202e85cca9fe89e624fbae3ceb1
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-20 01:58:28 +0100

    stub some SteamFriends methods

--- src/steam_api/friends.rs
+++ src/steam_api/friends.rs
@@ -4,6 +4,8 @@ use tracing::{info, debug, error};
 use vtables::VTable;
 use vtables_derive::{VTable, has_vtable};
 
+use crate::int32;
+
 #[has_vtable]
 #[derive(VTable, Debug)]
 pub struct SteamFriends {
@@ -15,13 +17,49 @@ impl SteamFriends {
   }
 }
 
+pub extern "fastcall" fn GetFriendCount(
+  self_: *mut SteamFriends,
+  _edx: *mut c_void,
+  friend_flags: int32,
+) -> int32 {
+  0
+}
+
+pub extern "fastcall" fn GetFriendByIndex(
+  self_: *mut SteamFriends,
+  _edx: *mut c_void,
+  friend: int32,
+  friend_flags: int32,
+) -> *mut c_void {
+  ptr::null_mut()
+}
+
+pub extern "fastcall" fn GetFriendsGroupCount(
+  self_: *mut SteamFriends,
+  _edx: *mut c_void,
+) -> int32 {
+  0
+}
+
 pub fn get_vtable() -> *mut *mut usize {
   unsafe {
-    static mut VTABLE: [*mut usize; 4] = [
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
+    static mut VTABLE: [*mut usize; 15] = [
+      ptr::null_mut(), // GetPersonaName
+      ptr::null_mut(), // SetPersonaName
+      ptr::null_mut(), // GetPersonaState
+      GetFriendCount as _, // GetFriendCount
+      GetFriendByIndex as _, // GetFriendByIndex
+      ptr::null_mut(), // GetFriendRelationship
+      ptr::null_mut(), // GetFriendPersonaState
+      ptr::null_mut(), // GetFriendPersonaName
+      ptr::null_mut(), // GetFriendGamePlayed
+      ptr::null_mut(), // GetFriendPersonaNameHistory
+      ptr::null_mut(), // GetFriendSteamLevel
+      ptr::null_mut(), // GetPlayerNickname
+      GetFriendsGroupCount as _, // GetFriendsGroupCount
+      ptr::null_mut(), // GetFriendsGroupIDByIndex
+      ptr::null_mut(), // GetFriendsGroupName
+      // ...
     ];
     VTABLE.as_mut_ptr()
   }
--- src/steam_client/get_generic_interfaces.rs
+++ src/steam_client/get_generic_interfaces.rs
@@ -3,7 +3,7 @@ use std::{os::raw::c_char, ffi::c_void, ptr};
 
 use tracing::debug;
 
-use crate::steam_client::{SteamAPI_ISteamClient_GetISteamApps, SteamAPI_ISteamClient_GetISteamInput, SteamAPI_ISteamClient_GetISteamParentalSettings, SteamAPI_ISteamClient_GetISteamUtils, SteamAPI_ISteamClient_GetISteamUserStats};
+use crate::steam_client::{SteamAPI_ISteamClient_GetISteamApps, SteamAPI_ISteamClient_GetISteamInput, SteamAPI_ISteamClient_GetISteamParentalSettings, SteamAPI_ISteamClient_GetISteamUtils, SteamAPI_ISteamClient_GetISteamUserStats, SteamAPI_ISteamClient_GetISteamFriends};
 use crate::{HSteamUser, HSteamPipe};
 
 use super::SteamClient;
@@ -26,12 +26,14 @@ pub unsafe extern "fastcall" fn SteamAPI_ISteamClient_GetISteamGenericInterface(
     return SteamAPI_ISteamClient_GetISteamUser(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   } else if version.starts_with("SteamUtils") {
     return SteamAPI_ISteamClient_GetISteamUtils(self_, _edx, hSteamPipe, pchVersion) as _;
-  } else if version.starts_with("STEAMAPPS_INTERFACE_VERSION") {
-    return SteamAPI_ISteamClient_GetISteamApps(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   } else if version.starts_with("SteamInput") {
     return SteamAPI_ISteamClient_GetISteamInput(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
+  } else if version.starts_with("SteamFriends") {
+    return SteamAPI_ISteamClient_GetISteamFriends(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   } else if version.starts_with("SteamNetworkingUtils") {
     return ptr::addr_of_mut!((*self_).steam_networking_utils) as _;
+  } else if version.starts_with("STEAMAPPS_INTERFACE_VERSION") {
+    return SteamAPI_ISteamClient_GetISteamApps(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   } else if version.starts_with("STEAMPARENTALSETTINGS_INTERFACE_VERSION") {
     return SteamAPI_ISteamClient_GetISteamParentalSettings(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   } else if version.starts_with("STEAMUSERSTATS_INTERFACE_VERSION") {
