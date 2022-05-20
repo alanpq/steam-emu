fed682edba90c32ed960c7b9fd771cabcf948af7
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-20 02:51:20 +0100

    stub SteamUGC::GetSubscribedItems

--- src/steam_api/ugc.rs
+++ src/steam_api/ugc.rs
@@ -4,6 +4,8 @@ use tracing::{info, debug, error};
 use vtables::VTable;
 use vtables_derive::{VTable, has_vtable};
 
+use crate::uint32;
+
 #[has_vtable]
 #[derive(VTable, Debug)]
 pub struct SteamUGC {
@@ -15,12 +17,87 @@ impl SteamUGC {
   }
 }
 
+pub extern "fastcall" fn GetSubscribedItems(
+  self_: *mut SteamUGC,
+  _edx: *mut c_void,
+  published_file_id: *mut c_void,
+  max_entries: uint32,
+) -> uint32 {
+  0
+}
+
 pub fn get_vtable() -> *mut *mut usize {
-  unsafe {
-    static mut VTABLE: [*mut usize; 4] = [
+  unsafe { // 69
+    static mut VTABLE: [*mut usize; 70] = [
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      GetSubscribedItems as _,
       ptr::null_mut(),
       // ...
     ];
--- src/steam_client/get_generic_interfaces.rs
+++ src/steam_client/get_generic_interfaces.rs
@@ -3,7 +3,7 @@ use std::{os::raw::c_char, ffi::c_void, ptr};
 
 use tracing::debug;
 
-use crate::steam_client::{SteamAPI_ISteamClient_GetISteamApps, SteamAPI_ISteamClient_GetISteamInput, SteamAPI_ISteamClient_GetISteamParentalSettings, SteamAPI_ISteamClient_GetISteamUtils, SteamAPI_ISteamClient_GetISteamUserStats, SteamAPI_ISteamClient_GetISteamFriends, SteamAPI_ISteamClient_GetISteamRemoteStorage, SteamAPI_ISteamClient_GetISteamHTMLSurface};
+use crate::steam_client::{SteamAPI_ISteamClient_GetISteamApps, SteamAPI_ISteamClient_GetISteamInput, SteamAPI_ISteamClient_GetISteamParentalSettings, SteamAPI_ISteamClient_GetISteamUtils, SteamAPI_ISteamClient_GetISteamUserStats, SteamAPI_ISteamClient_GetISteamFriends, SteamAPI_ISteamClient_GetISteamRemoteStorage, SteamAPI_ISteamClient_GetISteamHTMLSurface, SteamAPI_ISteamClient_GetISteamInventory, SteamAPI_ISteamClient_GetISteamUGC};
 use crate::{HSteamUser, HSteamPipe};
 
 use super::SteamClient;
@@ -42,6 +42,10 @@ pub unsafe extern "fastcall" fn SteamAPI_ISteamClient_GetISteamGenericInterface(
     return SteamAPI_ISteamClient_GetISteamRemoteStorage(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   } else if version.starts_with("STEAMHTMLSURFACE_INTERFACE_VERSION") {
     return SteamAPI_ISteamClient_GetISteamHTMLSurface(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
+  } else if version.starts_with("STEAMINVENTORY_INTERFACE") {
+    return SteamAPI_ISteamClient_GetISteamInventory(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
+  } else if version.starts_with("STEAMUGC_INTERFACE_VERSION") {
+    return SteamAPI_ISteamClient_GetISteamUGC(self_, _edx, hSteamUser, hSteamPipe, pchVersion) as _;
   }
   
   ptr::null_mut()
