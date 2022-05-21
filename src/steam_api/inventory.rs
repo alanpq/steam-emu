288a048c9714236ea82a7f216b52830743953059
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-21 21:41:00 +0100

    stub some SteamInventory methods

--- src/steam_api/inventory.rs
+++ src/steam_api/inventory.rs
@@ -5,6 +5,9 @@ use vtables::VTable;
 use vtables_derive::{VTable, has_vtable};
 
 use super::SteamAPICall_t;
+use crate::{uint32, int32};
+
+pub type SteamInventoryResult_t = int32;
 
 #[has_vtable]
 #[derive(VTable, Debug)]
@@ -15,6 +18,42 @@ impl SteamInventory {
   pub fn new() -> Self {
     Self { vtable: get_vtable() }
   }
+}
+
+// virtual bool GetAllItems( SteamInventoryResult_t *pResultHandle ) = 0;
+
+pub extern "fastcall" fn GetAllItems(
+  self_: *mut SteamInventory,
+  _edx: *mut c_void,
+  result_handle: *mut SteamInventoryResult_t,
+) -> bool {
+  false
+}
+
+pub extern "fastcall" fn GenerateItems(
+  self_: *mut SteamInventory,
+  _edx: *mut c_void,
+  result_handle: *mut c_void,
+  array_item_defs: *mut c_void,
+  array_quantity: *mut c_void,
+  array_length: uint32,
+) -> bool {
+  false
+}
+
+pub extern "fastcall" fn GrantPromoItems(
+  self_: *mut SteamInventory,
+  _edx: *mut c_void,
+  result_handle: *mut c_void,
+) -> bool {
+  false
+}
+
+pub extern "fastcall" fn LoadItemDefinitions(
+  self_: *mut SteamInventory,
+  _edx: *mut c_void,
+) -> bool {
+  false
 }
 
 pub extern "fastcall" fn RequestPrices(
@@ -33,6 +72,12 @@ pub fn get_vtable() -> *mut *mut usize {
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
+      GetAllItems as _, // GetAllItems
+      ptr::null_mut(),
+      ptr::null_mut(),
+      ptr::null_mut(),
+      GenerateItems as _, // GenerateItems
+      GrantPromoItems as _, // GrantPromoItems
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
@@ -41,13 +86,7 @@ pub fn get_vtable() -> *mut *mut usize {
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
+      LoadItemDefinitions as _, // LoadItemDefinitions
       ptr::null_mut(),
       ptr::null_mut(),
       ptr::null_mut(),
