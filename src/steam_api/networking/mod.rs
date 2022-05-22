6e5f2a85505336e20d93457827ba70d9303418a1
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-22 02:17:05 +0100

    add networking types

--- src/steam_api/networking/mod.rs
+++ src/steam_api/networking/mod.rs
@@ -2,8 +2,10 @@ mod messages;
 mod sockets;
 mod networking;
 mod networking_utils;
+mod types;
 
 pub use messages::*;
 pub use sockets::*;
 pub use networking::*;
 pub use networking_utils::*;
+pub use types::*;
---
+++ src/steam_api/networking/types.rs
@@ -0,0 +1,10 @@
+use crate::uint32;
+
+pub type SteamNetConnection = uint32;
+pub const k_HSteamNetConnection_Invalid: SteamNetConnection = 0;
+
+pub type SteamListenSocket = uint32;
+pub const k_HSteamListenSocket_Invalid: SteamNetConnection = 0;
+
+pub type SteamNetPollGroup = uint32;
+pub const k_HSteamNetPollGroup_Invalid: SteamNetConnection = 0;
