202a483f585fa65b7a24624e817e7bcee391be4f
Alan Panayotov <alanpanayotov@gmail.com>
2022-05-22 02:19:10 +0100

    stub some net socket methods

--- src/steam_api/networking/sockets.rs
+++ src/steam_api/networking/sockets.rs
@@ -4,6 +4,10 @@ use tracing::{info, debug, error};
 use vtables::VTable;
 use vtables_derive::{VTable, has_vtable};
 
+use crate::int32;
+
+use super::{SteamListenSocket, SteamNetPollGroup};
+
 #[has_vtable]
 #[derive(VTable, Debug)]
 pub struct SteamNetworkingSockets {
@@ -15,13 +19,52 @@ impl SteamNetworkingSockets {
   }
 }
 
+pub extern "fastcall" fn CreateListenSocketP2P(
+  self_: *mut SteamNetworkingSockets,
+  _edx: *mut c_void,
+  local_virtual_port: int32,
+  no_options: int32,
+  options: *mut c_void,
+) -> SteamListenSocket {
+  0 // FIXME: implement
+}
+
+pub extern "fastcall" fn CreatePollGroup(
+  self_: *mut SteamNetworkingSockets,
+  _edx: *mut c_void,
+) -> SteamNetPollGroup {
+  0 // FIXME: implement
+}
+
 pub fn get_vtable() -> *mut *mut usize {
   unsafe {
-    static mut VTABLE: [*mut usize; 4] = [
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
-      ptr::null_mut(),
+    static mut VTABLE: [*mut usize; 26] = [
+      ptr::null_mut(), // CreateListenSocketIP
+      ptr::null_mut(), // ConnectByIPAddress
+      CreateListenSocketP2P as _, // CreateListenSocketP2P
+      ptr::null_mut(), // ConnectP2P
+      ptr::null_mut(), // AcceptConnection
+      ptr::null_mut(), // CloseConnection
+      ptr::null_mut(), // CloseListenSocket
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      ptr::null_mut(), // 
+      CreatePollGroup as _, // CreatePollGroup
+      ptr::null_mut(), // DestroyPollGroup
       // ...
     ];
     VTABLE.as_mut_ptr()
